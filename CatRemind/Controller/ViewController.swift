e7bcad31898de1d7b2c8899c3b3a7aec21fd835d
Guillermo Ramos <memo33101@gmail.com>
2019-02-26 12:15:15 -0700

    Created project and organized into MVC

---
+++ CatRemind/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatRemind
+//
+//  Created by Guillermo Ramos  on 2/26/19.
+//  Copyright © 2019 Guillermo Ramos . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
