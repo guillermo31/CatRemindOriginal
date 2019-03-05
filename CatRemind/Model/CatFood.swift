f1ce7ca045af7435723e1d8182a52462a843981c
Guillermo Ramos <memo33101@gmail.com>
2019-03-05 12:07:03 -0700

    Created CatFood class

---
+++ CatRemind/Model/CatFood.swift
@@ -0,0 +1,20 @@
+//
+//  CatFood.swift
+//  CatRemind
+//
+//  Created by Guillermo Ramos  on 3/5/19.
+//  Copyright © 2019 Guillermo Ramos . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CatFood
+{
+    
+    
+    
+    
+    
+    
+}
